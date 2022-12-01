8c334dca19f1ca6ccb51ef2ff0c08743b7219e8c
Timothy Quan <timquan@pm.me>
2022-12-01 17:34:44 +0700

    first commit

---
+++ day_01-calorie counting.py
@@ -0,0 +1,14 @@
+elves = [0]
+with open('input.txt') as f:
+    for line in f.readlines():
+        if line == '\n':
+            elves.append(0)
+            continue
+        elves[-1] += int(line)
+
+# part 1
+elves = sorted(elves)
+print(f'The elf with the most has {elves[-1]} calories.')
+
+# part 2
+print(f'The top 3 elves have {sum(sorted(elves)[-2:])} calories total.')
